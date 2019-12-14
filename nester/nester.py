7e1970aec0de90f825dd83eab8e5f0fc63fc64b6
alex_dembo <ale.dembo@gmail.com>
2019-12-14 12:32:53 +0300

    add validation tests

--- nester/nester.py
+++ nester/nester.py
@@ -1,18 +1,27 @@
 class Nester:
     @staticmethod
+    def __validate(source_data, nesting_levels):
+        pass
+
+    @staticmethod
     def nest(source_data, nesting_levels):
+        Nester.__validate(source_data, nesting_levels)
         output = {}
         for data in source_data:
             nested_dict = output
             for nesting_level in nesting_levels:
+                data_nesting_level = data.pop(nesting_level)  # minimise lookups
                 if nesting_level != nesting_levels[-1]:
-                    if data[nesting_level] not in nested_dict:
-                        nested_dict[data[nesting_level]] = {}
-                    nested_dict = nested_dict[data[nesting_level]]
+                    if data_nesting_level not in nested_dict:
+                        nested_dict[data_nesting_level] = {}
+                    nested_dict = nested_dict[data_nesting_level]
                 else:  # final nesting
-                    if data[nesting_level] in nested_dict:
-                        nested_dict[data[nesting_level]].append(data)
+                    if data_nesting_level in nested_dict:
+                        nested_dict[data_nesting_level].append(data)
                     else:
-                        nested_dict[data[nesting_level]] = [data]
-                data.pop(nesting_level)
+                        nested_dict[data_nesting_level] = [data]
         return output
+
+    @staticmethod
+    def nest_json(source_data, nesting_levels):
+        pass
--- nester/tests.py
+++ nester/tests.py
@@ -22,3 +22,21 @@ class NesterTest(unittest.TestCase):
         expected = self.__load_json(output_json)
 
         self.assertEqual(expected, actual)
+
+    def test_nest_raises_invalid_key_error(self):
+        input_json = 'input/original_task.json'
+        nesting_levels = ['123']
+
+        self.assertRaises(KeyError, Nester.nest, self.__load_json(input_json), nesting_levels)
+
+    def test_nest_raises_invalid_value_error(self):
+        input_json = 'invalid json'
+        nesting_levels = ['123']
+
+        self.assertRaises(ValueError, Nester.nest_json, input_json, nesting_levels)
+
+        # passing invalid nesting levels
+        input_json = '{"valid":"json"}'
+        nesting_levels = []
+
+        self.assertRaises(ValueError, Nester.nest_json, input_json, nesting_levels)
